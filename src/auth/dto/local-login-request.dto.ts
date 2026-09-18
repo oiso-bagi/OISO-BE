82bfa122e75f1b38e2ace3285f5b74373e54f956
hdg0116 <hdg0116@gmail.com>
2026-09-19 02:25:47 +0900

    fix(auth): #136 로그인 ID 공백 입력 검증 추가

--- src/auth/dto/local-login-request.dto.ts
+++ src/auth/dto/local-login-request.dto.ts
@@ -1,11 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, MaxLength, MinLength } from 'class-validator';
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class LocalLoginRequestDto {
   @ApiProperty({
     description: '로컬 계정 로그인 아이디',
   })
   @IsString()
+  @Matches(/\S/, {
+    message: '로그인 아이디는 공백만 입력할 수 없습니다.',
+  })
   @MaxLength(255)
   email!: string;
 
